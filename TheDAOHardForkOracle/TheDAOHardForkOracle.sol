5840b7d9bd8917a8be4127f612aae2d31a1adb37
Noel Maersk <veox@wemakethings.net>
2016-07-17 19:04:15 +0300

    Add a different check as "fallback".
    Possible (forked,notforked) states:
    
    (false,false) | (false,true)
      DIDN'T RUN  |  NOT FORKED
    --------------+-------------
     (true,false) | (true,true)
        FORKED    |    ERROR
    
    Still has a lot of assumtions, but now also a "known to have failed"
    state.

--- TheDAOHardForkOracle/TheDAOHardForkOracle.sol
+++ TheDAOHardForkOracle/TheDAOHardForkOracle.sol
@@ -1,9 +1,11 @@
-contract daoHardForkOracle {
+contract TheDAOHardForkOracle {
     address constant WitdrawDAO = 0xbf4ed7b27f1d666546e30d74d50d173d20bca754;
+    address constant DarkDAO = 0x304a554a310c7e546dfe434669c62820b7d83490;
 
     // public, so accessors available
     bool public ran;
     bool public forked;
+    bool public notforked;
     
     modifier after_dao_hf_block {
 	if (block.number <= 1920000) throw;
@@ -24,7 +26,17 @@ contract daoHardForkOracle {
 	// 10M ether is ~ 2M less than would be available for a short
 	// while in WithdrawDAO after the HF, but probably more than
 	// anyone is willing to drop into WithdrawDAO in Classic
-	// ----- READABILITY --> 10*000*000 <-- 10 million
-	if (WithdrawDAO.balance >= 10000000 ether) forked = true;
+	// -- READABILITY -----> 10*000*000 <-- 10 million
+	if (WithdrawDAO.balance >= 10000000 ether)
+	    forked = true;
+
+	// failsafe: if the above assumption is incorrect, HF tine
+	// won't have balance in DarkDAO anyway, and Classic has a
+	// sliver of time before DarkDAO split happens
+	// -- READABILITY -> 3*000*000 <-------- 3 million
+	if (DarkDAO.balance >= 3000000 ether)
+	    notforked = true;
+
+	// if both flags are true, then something went wrong
     }
 }
