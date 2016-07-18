9860e06ec4b4a575d14b62e0e0a604975219c2af
Noel Maersk <veox@wemakethings.net>
2016-07-18 19:37:38 +0300

    deploy.js: clean up.

--- deploy.js
+++ deploy.js
@@ -1,4 +1,8 @@
+// manual deployment cheatsheet
 // replace `oracle` and `TheDAOHardForkOracle` with sed ;)
+
+// https://github.com/ethereum/go-ethereum/wiki/Contract-Tutorial
+
 // var oracle_src = '<paste code>'
 // var oracle_compiled = eth.compile.solidity(oracle_src)
 // var oracle_contract = eth.contract(oracle_compiled.TheDAOHardForkOracle.info.abiDefinition)
@@ -6,13 +10,13 @@
 var oracle = oracle_contract.new([],{from:eth.accounts[0], data: oracle_compiled.TheDAOHardForkOracle.code, gas: 200000}, function(e, contract){
     if (!e) {
         if(!contract.address) {
-            console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
+            console.log("Contract set, tx hash: " + contract.transactionHash);
+            console.log("Waiting to be mined...");
         } else {
-            console.log("Contract mined! Address: " + contract.address);
-            console.log(contract);
+            console.log("Done! Address: " + contract.address);
+            //console.log(contract);
         }
     } else {
-        console.log("Error!");
         console.log(e);
     }
 })
