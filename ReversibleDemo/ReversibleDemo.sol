edc65cba04af4b1523f3b269d53f4191c8758ac0
Noel Maersk <veox@wemakethings.net>
2016-08-07 22:23:04 +0300

    ReversibleDemo: `selfDestruct()` (with modifier) and other small changes.

--- ReversibleDemo/ReversibleDemo.sol
+++ ReversibleDemo/ReversibleDemo.sol
@@ -12,29 +12,41 @@ contract ReversibleDemo {
     uint public numfails;
     uint public numsuccesses;
 
+    address owner;
+
     event logCallFailed() {/* TODO */}
     event logCallSucceeded() {/* TODO */}
-    
-    modifier onlyThis {
-        if (msg.sender != this) throw;
-        _
+
+    modifier onlyOwner { if (msg.sender != owner) throw; _ }
+    modifier onlyThis { if (msg.sender != this) throw; _ }
+
+    // constructor (needed to allow termination)
+    function ReversibleDemo() {
+        owner = msg.sender;
     }
 
     // external -> increments EVM stack, even if invoked from same dapp
     // onlyThis -> not allowed by other accounts (external or dapps)
-    function callMe() external onlyThis returns (bool) {
+    function sendIfNotForked() external onlyThis returns (bool) {
         numcallsinternal++;
-        
-        if (!oracle.ran()) throw;
+
+        if (oracle.forked()) throw;
 
         return true;
     }
 
+    function selfDestruct() onlyOwner {
+        selfdestruct(owner);
+    }
+
     fuction() {
+        // accept value trasfers, but don't do anything
+        if (msg.value != 0) return;
+
         numcalls++;
 
         // TODO: limit gas?
-        bool ret = this.callMe();
+        bool ret = this.sendIfNotForked();
         
         if (!ret) {
             numfails++;
