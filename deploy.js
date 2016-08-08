d41118a7ab98ffc25f1798b53b5c1a162b91a685
Noel Maersk <veox@wemakethings.net>
2016-08-08 08:38:12 +0300

    deploy.js: sed-substitute for ReversibleDemo deployment.

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 // manual deployment cheatsheet
-// replace `oracle` and `TheDAOHardForkOracle` with sed ;)
+// replace `demo` and `ReversibleDemo` with sed ;)
 
 // https://github.com/ethereum/go-ethereum/wiki/Contract-Tutorial
 
-// var oracle_src = '<paste code>'
-// var oracle_compiled = eth.compile.solidity(oracle_src)
-// var oracle_contract = eth.contract(oracle_compiled.TheDAOHardForkOracle.info.abiDefinition)
+// var demo_src = '<paste code>'
+// var demo_compiled = eth.compile.solidity(demo_src)
+// var demo_contract = eth.contract(demo_compiled.ReversibleDemo.info.abiDefinition)
 
-var oracle = oracle_contract.new([],{from:eth.accounts[0], data: oracle_compiled.TheDAOHardForkOracle.code, gas: 200000}, function(e, contract){
+var demo = demo_contract.new([],{from:eth.accounts[0], data: demo_compiled.ReversibleDemo.code, gas: 200000}, function(e, contract){
     if (!e) {
         if(!contract.address) {
             console.log("Contract set, tx hash: " + contract.transactionHash);
