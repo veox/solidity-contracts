debb217bdad2281ea17e7394f07e9dfed2ca353d
Noel Maersk <veox@wemakethings.net>
2016-08-08 12:24:40 +0300

    deploy.js: `owneraddr` var, don't long-line.

--- deploy.js
+++ deploy.js
@@ -3,11 +3,16 @@
 
 // https://github.com/ethereum/go-ethereum/wiki/Contract-Tutorial
 
+// var owneraddr = "0x04b3faaa7c8127a80eb6d24672cfdaf4aecabbf8";
 // var demo_src = '<paste code>'
 // var demo_compiled = eth.compile.solidity(demo_src)
 // var demo_contract = eth.contract(demo_compiled.ReversibleDemo.info.abiDefinition)
 
-var demo = demo_contract.new([],{from:eth.accounts[0], data: demo_compiled.ReversibleDemo.code, gas: 200000}, function(e, contract){
+var demo = demo_contract.new([],
+                             {from: owneraddr,
+                              data: demo_compiled.ReversibleDemo.code,
+                              gas: 200000},
+                             function(e, contract){
     if (!e) {
         if(!contract.address) {
             console.log("Contract set, tx hash: " + contract.transactionHash);
