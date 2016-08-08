fd23351b7d868f3a66162163486d5ac25668c497
Noel Maersk <veox@wemakethings.net>
2016-08-08 13:06:39 +0300

    deploy.js: `solc` estimates creation gas at 294693.
    First deployment to 0x826786a70387f29d7e578ffb12cf560841019b9c failed
    due to insufficient gas. This is now a regular "external" account
    with 84 wei locked forever. :/

--- deploy.js
+++ deploy.js
@@ -3,6 +3,8 @@
 
 // https://github.com/ethereum/go-ethereum/wiki/Contract-Tutorial
 
+// solc --gas ReversibleDemo.sol
+
 // var owneraddr = "0x04b3faaa7c8127a80eb6d24672cfdaf4aecabbf8";
 // var demo_src = '<paste code>'
 // var demo_compiled = eth.compile.solidity(demo_src)
@@ -11,7 +13,7 @@
 var demo = demo_contract.new([],
                              {from: owneraddr,
                               data: demo_compiled.ReversibleDemo.code,
-                              gas: 200000},
+                              gas: 500000},
                              function(e, contract){
     if (!e) {
         if(!contract.address) {
